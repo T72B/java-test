62176277a46d6d71613ea46b02b1c92d372d608d
IGOR <ii311205@yandex.ru>
2015-09-07 14:42:16 +0300

    Have a little correction

--- java-test1/src/HelloWorld/HelloWorld.java
+++ java-test1/src/HelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello, World! I'm class HelloWorld.");
+			System.out.println("Hello, World! \nI'm class HelloWorld.");
 	}
 
 }
